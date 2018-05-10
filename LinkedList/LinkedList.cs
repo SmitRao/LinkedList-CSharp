eae5288a9b7e7af757588005bca62bfcb101f636
Smit Rao <smitrao@outlook.com>
2018-05-09 22:48:29 -0400

    Added LinkedList methods.

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -9,16 +9,51 @@ namespace LinkedList
 {
     class LinkedList
     {
-
-
-
-
-
-        public static void Main(string[] args)
+        public Node front;
+        public Node back;
+        public int size;
+        
+        public LinkedList(object item)
         {
-            Node node = new Node("Hello");
-            Debug.WriteLine("Value: " + node.value + " Next: " + node.next);
+            this.front = this.back = new Node(item);
+            this.size = 1;
+        }
 
+        public void Append(object toAppend)
+        {
+            this.back = new Node(toAppend);
+            this.front.next = this.back;
+            this.size++;
+        }
+
+        public void Prepend(object toPrepend)
+        {
+            Node newFront = new Node(toPrepend);
+            newFront.next = this.front;
+            this.front = newFront;
+            this.size++;
+        }
+
+        public void RemoveIndex(int index)
+        {
+            if(index >= this.size || index < 0)
+            {
+                throw new IndexOutOfRangeException("Oops... sorry, your index is out of range.");
+            }
+
+            if(this.size < 2)
+            {
+                this.front = this.back = null;
+                this.size--;
+                return;
+            }
+
+            ref = this.front;
+            for(int i = 0; i < index; i++)
+            {
+
+            }
+            --this.size;
         }
     }
 
