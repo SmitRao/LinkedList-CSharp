3e17b79474739d09578a2d17febeadd9c0f57e4a
Smit Rao <smitrao@outlook.com>
2018-05-09 22:09:09 -0400

    Added Node class and LinkedList in namespace

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+
+
+
+
+
+        public static void Main(string[] args)
+        {
+            Node node = new Node("Hello");
+            Debug.WriteLine("Value: " + node.value + " Next: " + node.next);
+
+        }
+    }
+
+    class Node
+    {
+        public object value;
+        public object next;
+
+        public Node(object value)
+        {
+            this.value = value;
+        }
+    }
+
+    
+}
